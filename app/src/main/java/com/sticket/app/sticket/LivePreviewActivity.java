7aee03c84e8b5ec8c41e1aab46bd6493d1d22dba
JaewanKim <wodhks6561@gmail.com>
2019-03-13 11:52:06 +0900

    [UPDATE] StickerDialog without Dim

--- app/src/main/java/com/sticket/app/sticket/LivePreviewActivity.java
+++ app/src/main/java/com/sticket/app/sticket/LivePreviewActivity.java
@@ -14,6 +14,7 @@
 package com.sticket.app.sticket;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
--- app/src/main/java/com/sticket/app/sticket/util/StickerDialog.java
+++ app/src/main/java/com/sticket/app/sticket/util/StickerDialog.java
@@ -1,5 +1,6 @@
 package com.sticket.app.sticket.util;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,44 +15,17 @@ import android.view.WindowManager;
 
 import com.sticket.app.sticket.R;
 
-import static com.sticket.app.sticket.util.Alert.context;
-
-
-//public class StickerDialog extends Activity {
-//
-//    private Context context;
-//
-//    public StickerDialog(Context context) {
-//        this.context = context;
-//    }
-//
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.dialog_sticker);
-//
-//    }
-//
-//    public void openDialog() {
-//        final Dialog dlg = new Dialog(context);
-//        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        dlg.setContentView(R.layout.dialog_sticker);
-//        dlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-//        dlg.setCanceledOnTouchOutside(true);
-//
-//        Window window = dlg.getWindow();
-//        WindowManager.LayoutParams params = window.getAttributes();
-//        window.setGravity(Gravity.BOTTOM);
-//        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//
-//        window.setAttributes(params);
-//        dlg.show();
-//    }
-//
-//
-//}
-
 public class StickerDialog extends BottomSheetDialogFragment {
+
+    @Override public void onStart() {
+        super.onStart();
+
+        Window window = getDialog().getWindow();
+        WindowManager.LayoutParams windowParams = window.getAttributes();
+        windowParams.dimAmount = 0;
+        windowParams.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        window.setAttributes(windowParams);
+    }
 
     @android.support.annotation.Nullable
     @Override
