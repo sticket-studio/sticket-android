444b1a2f231ea516450b8bca965546fff4a68a84
JaewanKim <wodhks6561@gmail.com>
2019-03-22 12:05:01 +0900

    Apply .gitignore

--- app/src/main/java/com/sticket/app/sticket/util/GridFragment.java
+++ app/src/main/java/com/sticket/app/sticket/util/GridFragment.java
@@ -1,15 +1,15 @@
 package com.sticket.app.sticket.util;
 
+
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
 import com.sticket.app.sticket.R;
+
 
 
 public class GridFragment extends Fragment{
@@ -20,12 +20,6 @@ public class GridFragment extends Fragment{
         // Required empty public constructor
     }
 
-//    // Q. Is it Redundant Code?
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,8 +27,6 @@ public class GridFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_grid, container, false);
 
         gridView = (GridView) view.findViewById(R.id.gridView);
-
         return view;
     }
-
 }
