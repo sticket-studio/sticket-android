01b822c167f2d1b522fd63acc7fa0af4e5425a78
yeoneei <soso3786@gmail.com>
2019-06-09 06:26:58 +0900

    [update] complete calling photoes

--- app/src/main/java/com/sticket/app/sticket/activities/sticker/asset_importer/AssetImporter.java
+++ app/src/main/java/com/sticket/app/sticket/activities/sticker/asset_importer/AssetImporter.java
@@ -4,42 +4,98 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
 import com.sticket.app.sticket.R;
+import com.sticket.app.sticket.activities.sticker.sticon_editor.SticonEditorActivity;
+import com.sticket.app.sticket.database.SticketDatabase;
+import com.sticket.app.sticket.database.entity.Asset;
+import com.sticket.app.sticket.database.entity.SticonAsset;
+import com.sticket.app.sticket.util.ImageViewUtil;
+import com.sticket.app.sticket.util.Landmark;
+import com.xiaopo.flying.sticker.BitmapStickerIcon;
+import com.xiaopo.flying.sticker.DrawableSticker;
+import com.xiaopo.flying.sticker.Sticker;
+import com.xiaopo.flying.sticker.StickerView;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 
 public class AssetImporter   extends AppCompatActivity implements View.OnClickListener {
 
     final  int PICTURE_REQUEST_CODE = 100;
+    private static final String TAG = AssetImporter.class.getSimpleName();
     ImageView goToGallery;
     ImageView selectedImg;
-    FrameLayout assetImpoerter;
+    BitmapStickerIcon sticker;
+    RelativeLayout assetImpoerter;
     AssetImporterAdapter assetImporterAdapter;
+    @BindView(R.id.btn_asset_editor_left_eye)
+    Button leftEyeBtn;
+    @BindView(R.id.btn_asset_editor_right_eye)
+    Button rightEyeBtn;
+    @BindView(R.id.btn_asset_editor_glasses)
+    Button GlassesBtn;
+    @BindView(R.id.btn_asset_editor_nose)
+    Button noseBtn;
+    @BindView(R.id.btn_asset_editor_left_cheek)
+    Button leftCheekBtn;
+    @BindView(R.id.btn_asset_editor_right_cheek)
+    Button rightCheekBtn;
+    @BindView(R.id.btn_asset_editor_mouth)
+    Button mouthBtn;
+    @BindView(R.id.sv_asset_editor_editor)
+    StickerView stickerView;
+    Bitmap bitmap;
+    Button currentBtn;
+
+
+    private SticketDatabase database;
+
 
     ArrayList<Uri> galleryUriList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_importer_asset);
-        assetImpoerter = (FrameLayout)findViewById(R.id.layout_asset_importer_frame);
+        assetImpoerter = (RelativeLayout) findViewById(R.id.layout_asset_editor_editor);
 
         RecyclerView recyclerView = findViewById(R.id.recyler_asset_impoter_gallery_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         galleryUriList = getVideo();
         assetImporterAdapter = new AssetImporterAdapter(galleryUriList);
         recyclerView.setAdapter(assetImporterAdapter);
-        selectedImg = (ImageView)findViewById(R.id.iv_asset_importer_selectedImg) ;
+        selectedImg = (ImageView) findViewById(R.id.iv_asset_importer_selectedImg) ;
         goToGallery = (ImageView) findViewById(R.id.iv_asset_importer_gotoGallery);
         goToGallery.setImageResource(R.drawable.img_gallery);
         goToGallery.setOnClickListener(new View.OnClickListener() {
@@ -55,12 +111,24 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
         assetImporterAdapter.setItemClick(new AssetImporterAdapter.ItemClick() {
             @Override
             public void onClick(View view, int position) {
-                selectedImg.setImageURI(galleryUriList.get(position));
-                selectedImg.setVisibility(View.VISIBLE);
-                selectedImg.bringToFront();
+                try {
 
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(Uri.parse("file://"+galleryUriList.get(position))));
+                    Sticker sticker = new DrawableSticker(new BitmapDrawable(getResources(), bitmap));
+                    stickerView.addSticker(sticker);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
             }
         });
+
+
+
+        ButterKnife.bind(this);
+
+        database = SticketDatabase.getDatabase(this);
+
+        currentBtn = leftEyeBtn;
     }
 
     private ArrayList<Uri> getVideo() {
@@ -75,6 +143,9 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
         int count=0;
         while(count<10){
             cursor.moveToNext();
+            Log.i("uri check",Uri.parse(cursor.getString(1)).toString());
+
+            Log.i("uri check",Uri.parse(cursor.getString(2)).toString());
             gelleryUriList.add(Uri.parse(cursor.getString(2)));
             count++;
         }
@@ -82,8 +153,10 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
         return gelleryUriList;
     }
 
-    @Override
+
     public void onClick(View v) {
+
+
     }
 
     @Override
@@ -93,15 +166,59 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
                 try{
                     InputStream in = getContentResolver().openInputStream(data.getData());
                     Bitmap img = BitmapFactory.decodeStream(in);
+                    bitmap = img;
+                    Sticker sticker = new DrawableSticker(new BitmapDrawable(getResources(), bitmap));
+                    stickerView.addSticker(sticker);
                     in.close();
-                    selectedImg.setImageBitmap(img);
-                    selectedImg.setVisibility(View.VISIBLE);
-                    selectedImg.bringToFront();
-
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }
     }
+    public Bitmap loadBitmap(String url)
+    {
+        Bitmap bm = null;
+        InputStream is = null;
+        BufferedInputStream bis = null;
+        try
+        {
+            URLConnection conn = new URL(url).openConnection();
+            conn.connect();
+            is = conn.getInputStream();
+            bis = new BufferedInputStream(is, 8192);
+            bm = BitmapFactory.decodeStream(bis);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if (bis != null)
+            {
+                try
+                {
+                    bis.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if (is != null)
+            {
+                try
+                {
+                    is.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bm;
+    }
+
+
 }
