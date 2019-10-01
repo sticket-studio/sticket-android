1a44a5ca9d390b2af3934df32c5eb1c9bd692c1f
yeoneei <soso3786@gmail.com>
2019-10-01 21:10:35 +0900

    temp

--- app/src/main/java/com/sticket/app/sticket/activities/sticker/asset_importer/AssetImporter.java
+++ app/src/main/java/com/sticket/app/sticket/activities/sticker/asset_importer/AssetImporter.java
@@ -10,6 +10,7 @@ import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
@@ -19,15 +20,21 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.Nullable;
+
 import com.sticket.app.sticket.R;
 import com.sticket.app.sticket.activities.sticker.sticon_editor.SticonEditorActivity;
+import com.sticket.app.sticket.activities.sticker.sticon_editor.SticonEditorViewPagerAdapter;
 import com.sticket.app.sticket.database.SticketDatabase;
 import com.sticket.app.sticket.database.entity.Asset;
+import com.sticket.app.sticket.database.entity.Sticon;
 import com.sticket.app.sticket.database.entity.SticonAsset;
+import com.sticket.app.sticket.util.FileUtil;
 import com.sticket.app.sticket.util.ImageViewUtil;
 import com.sticket.app.sticket.util.Landmark;
 import com.xiaopo.flying.sticker.BitmapStickerIcon;
@@ -44,11 +51,13 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
+import butterknife.OnClick;
 
 
 public class AssetImporter   extends AppCompatActivity implements View.OnClickListener {
@@ -78,9 +87,15 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
     StickerView stickerView;
     Bitmap bitmap;
     Button currentBtn;
-
-
+    private Map<Landmark, Sticker> stickerMap;
+    private Map<Sticker, Landmark> landmarkMap;
+    private Map<Landmark, Button> buttonMap;
+    private Map<Landmark, Asset> assetMap;
+    private Map<Landmark, Bitmap> bitmapMap;
+    private Landmark currentLandmark = Landmark.EYE_LEFT;
     private SticketDatabase database;
+
+
 
 
     ArrayList<Uri> galleryUriList = new ArrayList<>();
@@ -95,7 +110,7 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
         galleryUriList = getVideo();
         assetImporterAdapter = new AssetImporterAdapter(galleryUriList);
         recyclerView.setAdapter(assetImporterAdapter);
-        selectedImg = (ImageView) findViewById(R.id.iv_asset_importer_selectedImg) ;
+        //selectedImg = (ImageView) findViewById(R.id.iv_asset_importer_selectedImg) ;
         goToGallery = (ImageView) findViewById(R.id.iv_asset_importer_gotoGallery);
         goToGallery.setImageResource(R.drawable.img_gallery);
         goToGallery.setOnClickListener(new View.OnClickListener() {
@@ -125,10 +140,56 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
 
 
         ButterKnife.bind(this);
-
         database = SticketDatabase.getDatabase(this);
+        ButterKnife.bind(this);
+
+
+        landmarkMap = new HashMap<>();
+        stickerMap = new HashMap<>();
+        buttonMap = new HashMap<>();
+        assetMap = new HashMap<>();
+        bitmapMap = new HashMap<>();
 
         currentBtn = leftEyeBtn;
+        initListener();
+    }
+    @OnClick({R.id.btn_asset_editor_left_eye, R.id.btn_asset_editor_right_eye,
+            R.id.btn_asset_editor_glasses, R.id.btn_asset_editor_nose,
+            R.id.btn_asset_editor_left_cheek, R.id.btn_asset_editor_right_cheek
+            , R.id.btn_asset_editor_mouth})
+    public void onClick(View v) {
+        if (!stickerMap.containsKey(currentLandmark)) {
+            currentBtn.setBackground(getDrawable(R.drawable.btn_gray));
+        } else {
+            currentBtn.setBackground(getDrawable(R.drawable.btn_green));
+        }
+
+        currentBtn = (Button) v;
+        currentBtn.setBackground(getDrawable(R.drawable.btn_pink));
+
+        switch (v.getId()) {
+            case R.id.btn_asset_editor_left_eye:
+                currentLandmark = Landmark.EYE_LEFT;
+                break;
+            case R.id.btn_asset_editor_right_eye:
+                currentLandmark = Landmark.EYE_RIGHT;
+                break;
+            case R.id.btn_asset_editor_glasses:
+                currentLandmark = Landmark.GLASSES;
+                break;
+            case R.id.btn_asset_editor_left_cheek:
+                currentLandmark = Landmark.CHEEK_LEFT;
+                break;
+            case R.id.btn_asset_editor_right_cheek:
+                currentLandmark = Landmark.CHEEK_RIGHT;
+                break;
+            case R.id.btn_asset_editor_nose:
+                currentLandmark = Landmark.NOSE;
+                break;
+            case R.id.btn_asset_editor_mouth:
+                currentLandmark = Landmark.MOUTH;
+                break;
+        }
     }
 
     private ArrayList<Uri> getVideo() {
@@ -153,10 +214,64 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
         return gelleryUriList;
     }
 
+    private void initListener() {
+        stickerView.setOnStickerOperationListener(new StickerView.OnStickerOperationListener() {
+            @Override
+            public void onStickerClicked(Sticker sticker){
 
-    public void onClick(View v) {
+                Landmark landmark = landmarkMap.get(sticker);
 
+                if (sticker.equals(stickerMap.get(landmark))) {
+                    if (!stickerMap.containsKey(currentLandmark)) {
+                        currentBtn.setBackground(getDrawable(R.drawable.btn_gray));
+                    } else {
+                        currentBtn.setBackground(getDrawable(R.drawable.btn_green));
+                    }
 
+                    currentBtn = buttonMap.get(landmark);
+                    currentBtn.setBackground(getDrawable(R.drawable.btn_pink));
+                }
+            }
+
+            @Override
+            public void onStickerDeleted(Sticker sticker) {
+                Landmark landmark = landmarkMap.get(sticker);
+                if (sticker.equals(stickerMap.get(landmark))) {
+                    if (!currentBtn.equals(buttonMap.get(landmark))) {
+                        buttonMap.get(landmark).setBackground(getDrawable(R.drawable.btn_gray));
+                    }
+
+                    Log.e(TAG, "onStickerDeleted");
+                    stickerMap.remove(landmark);
+                    assetMap.remove(landmark);
+                    landmarkMap.remove(sticker);
+                }
+            }
+
+            @Override
+            public void onStickerDragFinished(Sticker sticker) {
+            }
+
+            @Override
+            public void onStickerZoomFinished(Sticker sticker) {
+            }
+
+            @Override
+            public void onStickerFlipped(Sticker sticker) {
+            }
+
+            @Override
+            public void onStickerDoubleTapped(Sticker sticker) {
+            }
+        });
+
+        buttonMap.put(Landmark.EYE_LEFT, leftEyeBtn);
+        buttonMap.put(Landmark.EYE_RIGHT, rightEyeBtn);
+        buttonMap.put(Landmark.GLASSES, GlassesBtn);
+        buttonMap.put(Landmark.CHEEK_LEFT, leftCheekBtn);
+        buttonMap.put(Landmark.CHEEK_RIGHT, rightCheekBtn);
+        buttonMap.put(Landmark.NOSE, noseBtn);
+        buttonMap.put(Landmark.MOUTH, mouthBtn);
     }
 
     @Override
@@ -175,49 +290,6 @@ public class AssetImporter   extends AppCompatActivity implements View.OnClickLi
                 }
             }
         }
-    }
-    public Bitmap loadBitmap(String url)
-    {
-        Bitmap bm = null;
-        InputStream is = null;
-        BufferedInputStream bis = null;
-        try
-        {
-            URLConnection conn = new URL(url).openConnection();
-            conn.connect();
-            is = conn.getInputStream();
-            bis = new BufferedInputStream(is, 8192);
-            bm = BitmapFactory.decodeStream(bis);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        finally {
-            if (bis != null)
-            {
-                try
-                {
-                    bis.close();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            if (is != null)
-            {
-                try
-                {
-                    is.close();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return bm;
     }
 
 
