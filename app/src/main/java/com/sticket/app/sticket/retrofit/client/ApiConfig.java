4ffc43e6e5be65750823c4e82d1ea8abb05fd79d
HeechanYang <yhc944@gmail.com>
2019-10-16 01:29:08 +0900

    [UPDATE] .gitignore

--- app/src/main/java/com/sticket/app/sticket/retrofit/client/ApiConfig.java
+++
@@ -1,7 +0,0 @@
-package com.sticket.app.sticket.retrofit.client;
-
-public class ApiConfig {
-    public static final String API_SERVER_HOST= "13.125.32.163";
-    public static final String USER_NAME= "sticket_id";
-    public static final String USER_SECRET= "sticket_secret";
-}
