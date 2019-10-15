6d7a0c96bdcc6756d2853e757c0ca17da54318a3
JaewanKim <wodhks6561@gmail.com>
2019-10-15 23:15:23 +0900

    [ADD] Add asset register activity & fragment in Store Register

---
+++ app/src/main/java/com/sticket/app/sticket/activities/store/store_register/StoreRegisterAssetActivity.java
@@ -0,0 +1,27 @@
+package com.sticket.app.sticket.activities.store.store_register;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.sticket.app.sticket.R;
+
+public class StoreRegisterAssetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_asset);
+    }
+
+    public void btnToBack(View view) {
+        onBackPressed();
+    }
+
+    public void btnToRegister(View view){
+        // TODO: 완료 버튼 눌렀을 때 일어날 일들
+
+//        Intent intent = new Intent(this, SigninActivity.class);
+//        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/sticket/app/sticket/activities/store/store_register/StoreRegisterFragment.java
@@ -0,0 +1,76 @@
+package com.sticket.app.sticket.activities.store.store_register;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sticket.app.sticket.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreRegisterFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_store_register, container, false);
+
+        ViewPager storeViewByAssetViewViewPager = (ViewPager) view.findViewById(R.id.store_register_viewpager);
+        setupViewPager(storeViewByAssetViewViewPager);
+
+        TabLayout storeViewByAssetViewTabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        storeViewByAssetViewTabLayout.setupWithViewPager(storeViewByAssetViewViewPager);
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFrag(new StoreRegisterViewFragment(), "눈");
+        adapter.addFrag(new StoreRegisterViewFragment(), "코");
+        adapter.addFrag(new StoreRegisterViewFragment(), "입");
+        adapter.addFrag(new StoreRegisterViewFragment(), "볼");
+        adapter.addFrag(new StoreRegisterViewFragment(), "귀");
+        viewPager.setAdapter(adapter);
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+}
